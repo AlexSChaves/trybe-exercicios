ce293492717b9206350fc89dbb952af304e0c387
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-08-30 16:23:13 -0300

    Iniciando o dia 7.2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,19 @@
+// const testingScope = escopo =>  {
+//   let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//   const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+  
+//   if (escopo === true) {
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(false);
+ 
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+
+
+console.log(`Os número ${oddsAndEvens.sort()} estão lindos`); // será necessário alterar essa linha 😉
